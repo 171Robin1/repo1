cbbb85136d181e98c9137e97abf681ff4b5ebadc
Marina <133687575+171Robin1@users.noreply.github.com>
2023-05-15 19:57:38 +0300

    Add files via upload

---
+++ 4A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+#define vll vector<long long>
+
+#define start ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#define range int t; cin >> t; while(t--)
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    start
+        cin >> n;
+        if(n%2||n==2) cout << "NO";
+        else cout<<"YES";
+}
+
